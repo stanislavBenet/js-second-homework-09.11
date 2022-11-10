4b4c0d649a9f0f9dbd05d92570321355c7ed5588
stanislavBenet <stanislav.benet@gmail.com>
2022-11-10 20:26:29 +0200

    Add correct version homework

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 let userInput1 = prompt("Please enter data");
 let userInput2 = prompt("Please enter data");
 
-isIfItNumberWriteInInputVariable(Number(userInput1));
-isIfItNumberWriteInInputVariable(Number(userInput2));
-
 function isCompareSimmilarType(val1, val2) {
   if (typeof val1 === typeof val2) {
     if (val1 > val2) {
@@ -17,12 +14,4 @@ function isCompareSimmilarType(val1, val2) {
   return null;
 }
 
-function isIfItNumberWriteInInputVariable(val1) {
-  if (isNaN(val1) || val1 === null || val1 === "") {
-    userInput1 = val1;
-  }
-}
-
 isCompareSimmilarType(userInput1, userInput2);
-
-console.log(isCompareSimmilarType(userInput1, userInput2));
